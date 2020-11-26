26d2952bb23c0d19c8ebd2b3c2fa689a68f6b4dd
vishal035 <kumarvishal880288@gmai;.com>
2020-11-26 10:56:26 +0530

    #Playground Folder has been removed from Git

--- playground/script.js
+++ playground/script.js
@@ -1,4 +1,59 @@
-const fs = require('fs');
+const tasks = {
+    tasks: [{
+        text: 'Grocery Shopping',
+        completed: true
+    },
+    {
+        text: 'Clean Yard',
+        completed: false
+    },
+    {
+        text: 'Film Course',
+        completed: false
+    }],
+    getTasksToDo() {
+        return incompletedTasks = this.tasks.filter((tasks) => tasks.completed === false);
+    }
+}
+
+
+
+console.log(tasks.getTasksToDo())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
 // const book = {
 //     title: "History",
 //     description: "Nothing Here"
@@ -12,21 +67,39 @@ const fs = require('fs');
 // console.log(JSON.parse(bookJson).title);
 
 
-const dataBuffer = fs.readFileSync('books.json');
-const dataJason = dataBuffer.toString();
-const data = JSON.parse(dataJason);
-console.log(data);
+// const dataBuffer = fs.readFileSync('books.json');
+// const dataJason = dataBuffer.toString();
+// const data = JSON.parse(dataJason);
+// console.log(data);
 
-data.name = "Vishal Kumar"
-data.age = 21
+// data.name = "Vishal Kumar"
+// data.age = 21
 
-console.log(data);
+// console.log(data);
 
-const dataEdited = JSON.stringify(data);
-fs.writeFileSync('books.json', dataEdited);
+// const dataEdited = JSON.stringify(data);
+// fs.writeFileSync('books.json', dataEdited);
 
 
-const dataBuffer1 = fs.readFileSync('books.json');
-const dataJason1 = dataBuffer1.toString();
-const data1 = JSON.parse(dataJason1);
-console.log(data1.name);
+// const dataBuffer1 = fs.readFileSync('books.json');
+// const dataJason1 = dataBuffer1.toString();
+// const data1 = JSON.parse(dataJason1);
+// console.log(data1.name);
+
+// const square = (x) => x*x;
+// console.log(square(2));
+
+// const objectss = {
+//     name: "Birthday Party",
+//     guestList:["Vishal", "Honey", "Alice"],
+//     print: function () {
+//         // const that = this
+//         console.log("Guest List For "+ this.name);
+
+//         this.guestList.forEach( (guest) => {
+//             console.log(guest + " is invited in "+ this.name);
+//         })
+//     }
+// }
+
+// objectss.print();
